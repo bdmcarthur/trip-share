07a0a7d9d3cc05fbaed69bf677a8571ece1ef17d
Brandy McArthur <bdm.mcarthur@gmail.com>
2021-05-16 12:16:53 +0100

    heroku

---
+++ client/build/precache-manifest.271e77331380b395c252bca791cff783.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62dc7cf560cb023c0ef0eda22f6d5ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1462a11e12e2bdfcf315",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d9c16bac42a67fe7fbae",
+    "url": "/static/css/main.35da9da7.chunk.css"
+  },
+  {
+    "revision": "1462a11e12e2bdfcf315",
+    "url": "/static/js/2.3c7c50b0.chunk.js"
+  },
+  {
+    "revision": "4a35bd8c30bb51952c911bad086749bd",
+    "url": "/static/js/2.3c7c50b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9c16bac42a67fe7fbae",
+    "url": "/static/js/main.ec46b20a.chunk.js"
+  },
+  {
+    "revision": "076a83d847071d9b5b3d",
+    "url": "/static/js/runtime-main.5a476709.js"
+  },
+  {
+    "revision": "663ddf8877e9dba6cedd0f6038f6f4ca",
+    "url": "/static/media/2022.663ddf88.png"
+  }
+]);
